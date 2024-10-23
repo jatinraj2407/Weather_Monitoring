fb19541d12d6321e4f2ab5f74daa1901eddeab03
T JATIN RAJ <98451299+jatinraj2407@users.noreply.github.com>
2024-10-23 18:37:34 +0530

    Create Visualization.py

---
+++ Visualization.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from database import Database
+
+def plot_weather_summary():
+    db = Database()
+    # Fetch data and plot
+    # Example plot code
+    plt.show()
